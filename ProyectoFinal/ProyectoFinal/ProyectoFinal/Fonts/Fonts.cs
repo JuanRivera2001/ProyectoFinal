1c6f3d2aa666e9e22f6c48d095295abf8cafcacc
JuanRivera991 <juan.rivera3@utp.edu.co>
2021-11-04 19:07:28 -0500

    startProject

---
+++ ProyectoFinal/ProyectoFinal/ProyectoFinal/Fonts/Fonts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoFinal.Fonts
+{
+    public static class Fonts
+    {
+		public const string EyeOff = "\u2699";
+		public const string Mail = "\ue803";
+		public const string LockOpenAlt1 = "\uf075";
+		public const string MailAlt = "\uf0e0";
+		public const string Mobile = "\uf10b";
+		public const string LockOpenAlt = "\uf13e";
+		public const string User = "\U0001f304";
+		public const string Eye = "\U0001f4c4";
+		public const string Lock = "\U0001f517";
+	}
+}
